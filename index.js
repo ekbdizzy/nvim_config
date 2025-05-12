bece488cdf78394d216107895bdc260591f41f4f
Алексей Кошкин <user@Aleksejs-MacBook-Pro.local>
2025-05-12 23:40:47 +0200

    Add hotkeys to speed navigate

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const toggleLog = (arg) => {
   console.log(arg);
 };
 
-conct a
 
--- lua/koshkin/core/keymaps.lua
+++ lua/koshkin/core/keymaps.lua
@@ -21,3 +21,30 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Resize windows with Alt + Arrow keys
+vim.keymap.set("n", "<M-Left>", "<cmd>vertical resize -4<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Right>", "<cmd>vertical resize +4<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Down>", "<cmd>resize -4<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Up>", "<cmd>resize +4<CR>", { noremap = true, silent = true })
+
+-- Navigate 4x faster when holding down Ctrl in normal mode
+vim.keymap.set("n", "<C-j>", "4j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "4k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "4l", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "4h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>", "4<Down>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>", "4<Up>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", "b", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Right>", "e", { noremap = true, silent = true })
+
+-- For visual mode: x[nore]map
+vim.keymap.set("x", "<C-j>", "4j", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-k>", "4k", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-h>", "4h", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-l>", "4l", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-Down>", "4<Down>", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-Up>", "4<Up>", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-Left>", "b", { noremap = true, silent = true })
+vim.keymap.set("x", "<C-Right>", "e", { noremap = true, silent = true })
+
