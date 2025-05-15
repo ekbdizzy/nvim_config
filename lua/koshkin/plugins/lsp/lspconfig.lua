01db59fa684055f442b10cdf2489e28abe25ab76
Алексей Кошкин <user@192.168.1.13>
2025-05-15 16:13:16 +0200

    Update deprecated setup_handlers

--- lua/koshkin/plugins/lsp/lspconfig.lua
+++ lua/koshkin/plugins/lsp/lspconfig.lua
@@ -115,84 +115,85 @@ return {
         "ruff",
       },
       automatic_installation = true,
+      handlers = {
+        {
+          -- default handler for installed servers
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
+          ["pyright"] = function()
+            -- Configure pyright server
+            lspconfig["pyright"].setup({
+              capabilities = capabilities,
+              settings = {
+                python = {
+                  analysis = {
+                    typeCheckingMode = "basic",
+                    diagnosticMode = "workspace",
+                    inlayHints = { variableTypes = true, functionReturnTypes = true },
+                    autoSearchPaths = true,
+                    useLibraryCodeForTypes = true,
+                  },
+                },
+              },
+            })
+          end,
+          ["ts_ls"] = function()
+            -- Configure tsserver
+            lspconfig["ts_ls"].setup({
+              capabilities = capabilities,
+            })
+          end,
+          ["svelte"] = function()
+            -- configure svelte server
+            lspconfig["svelte"].setup({
+              capabilities = capabilities,
+              on_attach = function(client, bufnr)
+                vim.api.nvim_create_autocmd("BufWritePost", {
+                  pattern = { "*.js", "*.ts" },
+                  callback = function(ctx)
+                    -- Here use ctx.match instead of ctx.file
+                    client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                  end,
+                })
+              end,
+            })
+          end,
+          ["graphql"] = function()
+            -- configure graphql language server
+            lspconfig["graphql"].setup({
+              capabilities = capabilities,
+              filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+            })
+          end,
+          ["emmet_ls"] = function()
+            -- configure emmet language server
+            lspconfig["emmet_ls"].setup({
+              capabilities = capabilities,
+              filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+            })
+          end,
+          ["lua_ls"] = function()
+            -- configure lua server (with special settings)
+            lspconfig["lua_ls"].setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  -- make the language server recognize "vim" global
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                  completion = {
+                    callSnippet = "Replace",
+                  },
+                },
+              },
+            })
+          end,
+        },
+      },
     })
-
-  mason_lspconfig.setup_handlers({
-    -- default handler for installed servers
-    function(server_name)
-      lspconfig[server_name].setup({
-        capabilities = capabilities,
-      })
-    end,
-    ["pyright"] = function()
-      -- Configure pyright server
-      lspconfig["pyright"].setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              typeCheckingMode = "basic",
-              diagnosticMode = "workspace",
-              inlayHints = { variableTypes = true, functionReturnTypes = true },
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-            },
-          },
-        },
-      })
-    end,
-    ["ts_ls"] = function()
-      -- Configure tsserver
-      lspconfig["ts_ls"].setup({
-        capabilities = capabilities,
-      })
-    end,
-    ["svelte"] = function()
-      -- configure svelte server
-      lspconfig["svelte"].setup({
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = { "*.js", "*.ts" },
-            callback = function(ctx)
-              -- Here use ctx.match instead of ctx.file
-              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-            end,
-          })
-        end,
-      })
-    end,
-    ["graphql"] = function()
-      -- configure graphql language server
-      lspconfig["graphql"].setup({
-        capabilities = capabilities,
-        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-      })
-    end,
-    ["emmet_ls"] = function()
-      -- configure emmet language server
-      lspconfig["emmet_ls"].setup({
-        capabilities = capabilities,
-        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-      })
-    end,
-    ["lua_ls"] = function()
-      -- configure lua server (with special settings)
-      lspconfig["lua_ls"].setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            -- make the language server recognize "vim" global
-            diagnostics = {
-              globals = { "vim" },
-            },
-            completion = {
-              callSnippet = "Replace",
-            },
-          },
-        },
-      })
-    end,
-  })
   end,
 }
